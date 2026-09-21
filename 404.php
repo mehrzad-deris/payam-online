e5480f56d96e2f1c2ad0d89384d7df47c7e305bf
Mehrzad Deris <mehrzad.deris@gmail.com>
2026-09-21 10:13:18 +0330

    test deploy

--- 404.php
+++ 404.php
@@ -44,6 +44,7 @@ $builderSource = payam_options_builder_source( 'error_404_builder' );
                 <span>بازگشت به صفحه اصلی</span>
                 <?= icon('arrow-linear-2', 'w-5 h-5 fill-white hover:rotate-45 duration-200') ?>
             </a>
+
     </div>
     </div>
 	<?php if ( payam_builder_sections( $builderSource ) ) : ?>
